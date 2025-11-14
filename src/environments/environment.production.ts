0717ba3e99a460214ffc48e90be8df356cb50fb9
Ross Bryson <jacobrbryson@gmail.com>
2025-11-14 12:25:57 -0500

    Minor updates

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   assetBase: 'https://storage.googleapis.com/assets-athena-app/',
+  proxyServer: 'https://athena-proxy-api-12367074465.us-central1.run.app',
 };
